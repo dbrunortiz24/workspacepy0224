83e1d048bb133bbb0a5220719b67c520288442cd
Danilo <160366180+dbrunortiz24@users.noreply.github.com>
2024-03-12 15:28:08 -0500

    Update listaEjercicios.py

--- modulo3/practica/listaEjercicios.py
+++ modulo3/practica/listaEjercicios.py
@@ -3,6 +3,45 @@
 #1.Una tienda de autopartes necesita un programa para catalogar sus productos,crear la clase catalogo
 # y producto, realizar un objeto dentro de un catalgo de productos el cual debe tener un metodo para agregar
 # productos el cual debe tener un metodo para agregar productos y otra para mostrar toda la lista de productos.
+class Producto:
+    def __init__(self, nombre, precio):
+        self.nombre = nombre
+        self.precio = precio
+
+# Creamos una lista para almacenar los productos
+catalogo = []
+
+# Método para agregar un producto al catálogo
+def agregar_producto():
+    nombre = input("Ingrese el nombre del producto: ")
+    precio = float(input("Ingrese el precio del producto: "))
+    producto = Producto(nombre, precio)
+    catalogo.append(producto)
+    print(f"Producto '{nombre}' agregado al catálogo.")
+
+# Método para mostrar toda la lista de productos
+def mostrar_catalogo():
+    print("\nCatálogo de productos:")
+    for producto in catalogo:
+        print(f"Nombre: {producto.nombre} | Precio: ${producto.precio:.2f}")
+
+# Menú principal
+while True:
+    print("\n--- Menú ---")
+    print("1. Agregar producto")
+    print("2. Mostrar catálogo")
+    print("3. Salir")
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+        agregar_producto()
+    elif opcion == "2":
+        mostrar_catalogo()
+    elif opcion == "3":
+        print("¡Hasta luego!")
+        break
+    else:
+        print("Opción inválida. Intente nuevamente.")
 
 #2. Realizar un programa que tenga una clase producto el cual solo tiene los atributos de nombre y codigo
 # el codigo tiene la siguiente estructura PAIS-LOTE-AÑO ejemplo:PERU-00001-2024 crear un metodo que imprima el objeto de forma 
